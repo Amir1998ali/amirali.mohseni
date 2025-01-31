51b1039e61201c70700be4271d3aab5de8745463
Amirali <amiralm@uci.edu>
2025-01-30 17:19:39 -0800

    Fix: Removed unused imports in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Lottie from 'react-lottie';
 import { motion } from 'framer-motion';
 import animationData from './animation.json';
